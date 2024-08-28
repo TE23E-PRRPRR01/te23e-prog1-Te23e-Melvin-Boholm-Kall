7a96167740e8153851ffe589bf58a5f3f677fa64
Melvin Boholm Käll <Melvin.boholmkall@elev.ga.ntig.se>
2024-08-28 14:42:29 +0200

    lucktext

---
+++ HelloWorld/kapitel 1/lucktext/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.IO.Compression;
+
+Console.Clear();
+Console.WriteLine("Vad är ditt namn?");
+string namn = Console.ReadLine();
+Console.WriteLine("Vad har du för kön?");
+string kön = Console.ReadLine();
+Console.WriteLine("Vad gillar du för mat? ");
+string mat = Console.ReadLine();
+Console.WriteLine("Vart gillar du spendera din tid?");
+string plats = Console.ReadLine();
+Console.WriteLine("Vart kommer du ifrån");
+string land = Console.ReadLine();
+
+Console.Clear();
+
+if ((land == "sverige") ||  (land == "Sverige"))
+{
+
+    Console.WriteLine($"{namn} är en liten {kön} som gillar att käka {mat} och vara {plats} för att man kommer från det underbara landet {land}.");
+
+
+} 
+else
+{
+
+    Console.WriteLine($"{namn} är en liten {kön} som gillar att käka {mat} och vara {plats} för att man kommer från {land}. Dem som kommer från {land} är lite lustiga och gillar att bete sig på det viset.");
+
+
+}
+
+Console.ReadKey();
