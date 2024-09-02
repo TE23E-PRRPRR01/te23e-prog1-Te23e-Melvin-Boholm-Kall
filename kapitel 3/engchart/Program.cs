427c3ef13aa20daa9de44eaac6f5aabf0b9f763b
Melvin Boholm Käll <Melvin.boholmkall@elev.ga.ntig.se>
2024-09-02 14:51:33 +0200

    Tränat på if-satser

---
+++ kapitel 3/engchart/Program.cs
@@ -0,0 +1,61 @@
+﻿// Program som simulerar kända engineering flowchart
+Console.Clear();
+Console.WriteLine("Välkommer till ingenjörens flödesschema");
+
+
+Console.WriteLine($"""
+
+                           -   ,--_--.
+               -           \      `.
+                      -     "-_ _   \
+     -                         / F   )
+                   -     -    / / `--'
+              -              / /
+                   -        / /
+            -            __/ /
+                        /,-pJ
+           -        _--"-L ||
+                  ,"      "//
+     -           /  ,-'''.//\
+                /  /     // J____
+               J  /     // L/----\
+   .           F J     //__//^---'
+     `     ___J  F    '----| |
+  `       J---|  F         F F
+`   `. `   `--J  L        J  F
+    .   .`     L J       J  F
+       .  .    J  \    ,"  F
+         .  `.` \  "--"  ,"
+            ` ``."-____-"
+
+""");
+
+Console.WriteLine("Does it move ? (y/n)");
+string answer = Console.ReadLine();
+
+if (answer == "y")
+{
+    Console.Write("Should it? (y/n)");
+    string shouldIT = Console.ReadLine();
+    if (shouldIT == "y")
+    {
+        Console.WriteLine("No problem");
+    }
+    else
+    {
+        Console.WriteLine("duct tape");
+    }
+}
+else
+{
+    Console.Write("Should it? (y/n)");
+    string shouldIt = Console.ReadLine();
+    if (shouldIt == "y")
+    {
+        Console.WriteLine("Please use WD-40");
+    }
+    else
+    {
+        Console.WriteLine("No problem");
+    }
+}
---
+++ kapitel 3/interactivStory/Program.cs
@@ -0,0 +1,47 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Clear();
+
+int health = 100;
+
+Console.WriteLine("Du står i ett rum, ett svärd ligger på golvet. Plockar du upp det? (y/n)");
+string answer = Console.ReadLine();
+if (answer == "y")
+{
+    Console.WriteLine("Du plockar upp svärdet och kollar dig runtomkring, du är i ett murkt rum med två dörrar. Går du in i dörr 1 eller 2? (1/2)");
+    answer = Console.ReadLine();
+    if (answer == "1")
+    {
+        Console.WriteLine("Du öppnar dörr två och ser en gigantisk spindel. Dräper du den? (y/n)");
+        answer = Console.ReadLine();
+        if (answer == "y")
+        {
+            Console.WriteLine("Du dräpte spindeln, men inte utan skada. Du förlorar 30 liv.");
+            health -= 30;
+            Console.WriteLine("Du står inför 2 dörrar ännu en gång. Vilken dörr går du igenom? (1/2)");
+            answer = Console.ReadLine();
+            if (answer == "1")
+            {
+                Console.WriteLine("Du öppnar dörr ett fast finner inget. Du tar ett par steg framåt för att vara 100% säker att rummet var tomt.");
+                Console.WriteLine("Du hör durren stängas bakom dig, du är fast i det här rummet. Du letar efter en utväg fast finner ingen, du dör av svält");
+                Environment.Exit(0);
+            }
+            else
+            {
+                
+            }
+
+        }
+        else
+        {
+            
+        }
+    }
+    else
+    {
+        
+    }
+}
+else
+{
+    
+}
