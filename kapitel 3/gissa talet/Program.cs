effaf74a10f7474d4a38585d0960aee8d703bd1a
Melvin Boholm Käll <Melvin.boholmkall@elev.ga.ntig.se>
2024-09-09 14:50:32 +0200

    Tränat på heltal och konverteringar.

---
+++ kapitel 3/gissa talet/Program.cs
@@ -0,0 +1,49 @@
+﻿// Gissa talet spel
+
+Console.Clear();
+
+Random randNummer = new Random();
+
+int nummer = randNummer.Next(1, 101);
+
+int försök = 0;
+
+
+while (true)
+{
+    Console.Write("Gissa ett tal 1-100: ");
+
+    string gissningText = Console.ReadLine();
+    int gissniing = int.Parse(gissningText);
+
+    if (gissniing == nummer)    
+    {
+        Console.Clear();
+        Console.WriteLine("Du gissa rätt!!");
+        Console.WriteLine($"Försök: {försök}");
+        
+        Environment.Exit(0);
+
+    }
+    else
+    {
+        Console.Clear();
+        Console.WriteLine("Du gissa fel :(");
+
+        försök += 1;
+
+        Console.WriteLine($"Försök: {försök}");
+
+        if (gissniing > nummer)
+        {
+
+            Console.WriteLine("Ditt gissa för högt");
+
+        }
+        else
+        {
+            Console.WriteLine("Du gissa för lågt");
+        }
+    }
+
+}
---
+++ kapitel 3/åldersgränser/Program.cs
@@ -0,0 +1,25 @@
+﻿// Åldersgränser
+Console.Clear();
+
+Console.WriteLine("Ange din längd i cm: ");
+string längdText = Console.ReadLine();
+
+int längd = int.Parse(längdText);
+
+
+if (längd < 110)
+{
+    Console.WriteLine("Du får inte åka något eftersom du är för kort.");
+}
+else if (längd >= 110 && längd < 140)
+{
+    Console.WriteLine("Då får du åka bergochdalbanorna för små barn.");
+}
+else if (längd >= 140 && längd <= 200)
+{
+    Console.WriteLine("Då får du åka alla bergochdalbanorna.");
+}
+else if (längd > 200)
+{
+    Console.WriteLine("Du får inte åka någon bergochdalbana eftersom du är för lång.");
+}
