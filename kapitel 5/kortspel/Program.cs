8152663d29772a68d3adc6dead27f04473b946f3
Melvin Boholm Käll <Melvin.boholmkall@elev.ga.ntig.se>
2024-09-30 14:53:07 +0200

    Tränat på list och slump, unicode, emoji

---
+++ kapitel 5/kortspel/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Clear();
+
+Console.WriteLine("Slumpa kort ur en kortlek");
+
+//Skapa en lista kort
+List<string> kortlek = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Kn", "D", "K"];
+
+
+int antal = 0;
+
+while(antal != 5)
+{
+int index = Random.Shared.Next(0, kortlek.Count);
+
+//Plocka ut det sjunde kortet
+string kort = kortlek[index];
+
+kortlek.RemoveAt(index);
+
+Console.WriteLine($"Det slumpade kortet är: {kort}");
+antal ++;
+}
---
+++ kapitel 5/städer/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Clear();
+
+Console.WriteLine("Slumpa stad ur städer");
+
+//Skapa en lista av städer
+List<string> städer = ["Stockholm", "New York City", "Chicago", "New Delhi", "London", "Tokyo", "Beijing", "Rio de janiero", "Riyadh", "versailles"];
+
+
+int antal = 0;
+
+while(antal != 5)
+{
+int index = Random.Shared.Next(0, 9);
+
+string stad = städer[index];
+
+Console.WriteLine($"Den slumpade staden är: {stad}");
+antal ++;
+}
