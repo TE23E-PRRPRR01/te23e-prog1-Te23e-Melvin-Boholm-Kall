f3b7d3c261f1c7ff329f9f201141476fac9440d8
Melvin Boholm Käll <Melvin.boholmkall@elev.ga.ntig.se>
2024-10-14 14:49:56 +0200

    Gjort lite allt möjligt

---
+++ kapitel 5/Listor/Program.cs
@@ -0,0 +1,62 @@
+﻿// Använda listor för skapa/hantera samlingar
+Console.Clear();
+
+// Lista på trevliga frukter, tex "äpple", "apelsin", "annanas"
+List<string> frukter = ["äpple", "apelsin", "annanas"];
+
+Console.WriteLine($"Min fruktlista: {frukter[0]}, {frukter[1]}, {frukter[2]}");
+
+// Ändra i listan
+
+frukter[0] = "banan";
+
+Console.WriteLine($"Min fruktlista: {frukter[0]}, {frukter[1]}, {frukter[2]}");
+
+// Gå igenom listan = loopa igenom = från första till sista
+
+foreach (var frukt in frukter)
+{
+    Console.WriteLine(frukt);
+}
+
+// Skapa en lista på valfri sak (4st)
+// Skriv ut varje sak på samma rad
+
+List<string> albums = ["The College Dropout", "Late Registration", "Graduation", "808s & Heartbrake"];
+
+foreach (var album in albums)
+{
+    Console.Write($"{album}, ");
+}
+
+for (int i = 0; i < albums.Count; i++)
+{
+    Console.Write($"{albums[i]} ");
+}
+
+// Skapa ett register
+// En tom lista
+
+Console.Clear();
+
+List<string> namnLista = [];
+
+// While-loop
+
+while (true)
+{
+    System.Console.Write("\nAnge ett namn: ");
+    string namn = Console.ReadLine();
+
+    namnLista.Add(namn);
+
+    // Skriv ut hela listan på en rad
+
+    foreach (var namnet in namnLista)
+    {
+        Console.Write($"{namnet}, ");
+    }
+
+
+    
+}
---
+++ kapitel 5/NamnRegister/Program.cs
@@ -0,0 +1,85 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Clear();
+Console.WriteLine("Ett litet namnregister");
+
+
+while (true)
+{
+    Console.Clear();
+
+    // Visa en meny
+    Console.WriteLine("""
+    1. Registrera namn
+    2. Skriva ut alla nman
+    3. Töm registret
+    4. Avsluta
+    Ange ditt val: 
+    """);
+
+    string val = Console.ReadLine().ToLower().Trim();
+
+    if (val == "1" || val == "registrera namn")
+    {
+        Console.Clear();
+        Console.Write("Vad heter personen? ");
+        string namn = Console.ReadLine();
+        File.AppendAllText($"namn.txt", namn + ", ");
+
+    }
+    else if (val == "2" || val == "skriva ut alla namn")
+    {
+        try
+        {
+            Console.Clear();
+            string text = File.ReadAllText("namn.txt");
+            if (text == "")
+            {
+                Console.WriteLine("Registret är tomt.");
+                Thread.Sleep(2000);
+
+            }
+            else
+            {
+                Console.WriteLine($"{text}");
+                Console.Write("Vill du fortsätta? (j/n) ");
+                val = Console.ReadLine();
+                if (val == "j")
+                {
+                    Console.Clear();
+                }
+                else
+                {
+                    Environment.Exit(0);
+                }
+            }
+
+
+
+        }
+        catch (Exception ex)
+        {
+            System.Console.Write("Det finns ingen namnlista ännu.");
+            System.Threading.Thread.Sleep(2000);
+            Console.Clear();
+        }
+    }
+    else if (val == "3" || val == "töm registret")
+    {
+        File.WriteAllText($"namn.txt", "");
+        Console.WriteLine("Registret har tömts.");
+        Thread.Sleep(2000);
+    }
+    else if (val == "4" || val == "avsluta")
+    {
+        Console.WriteLine("Adjö");
+        Thread.Sleep(1000);
+        Environment.Exit(0);
+    }
+    else
+    {
+        Console.WriteLine("Skriv i rätt format.");
+        Thread.Sleep(2000);
+        Console.Clear();
+    }
+
+}
